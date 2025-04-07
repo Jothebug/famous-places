de67d528cf12299ff0dbfe602bd36d74f30dd027
Jothebug <hayenbg2603@gmail.com>
2025-04-07 13:14:19 +0700

    Initial Commit

---
+++ famous-places/famous_placesApp.swift
@@ -0,0 +1,17 @@
+//
+//  famous_placesApp.swift
+//  famous-places
+//
+//  Created by HaYen on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct famous_placesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
